0d1ed3bbd673b9118faae6f271ba1da81491c26e
Johannes Geisslinger <johannes.geisslinger@gwce.de>
2020-11-02 13:28:36 +0100

    changed logic

--- Installer.sh
+++ Installer.sh
@@ -44,9 +44,9 @@ function SetupHostname {
 
 #Function to setup the static IP in dhcpcd
 function SetStaticIP {
-    
+    act_IP=$(hostname -I)
     while [[ -z $net_result ]] || [[ $net_result == "1" ]] ; do
-    IP=$(whiptail --inputbox --nocancel "IP Adress for your system" 8 78 Name --title "Network Settings" 3>&1 1>&2 2>&3)
+    IP=$(whiptail --inputbox --nocancel "IP Adress for your system" 8 78 $act_IP --title "Network Settings" 3>&1 1>&2 2>&3)
     GATEWAY=$(whiptail --inputbox --nocancel "Please enter the Gateway of your network" 8 78 Name --title "Network Setting" 3>&1 1>&2 2>&3)
     whiptail --title "Are the settings correct?" --yesno "\n IP Adress: $IP \n Gateway: $GATEWAY \n" 18 78 3>&1 1>&2 2>&3
     net_result=$?
@@ -72,7 +72,7 @@ function WriteIP {
 
 function SetRootPW 
     {
-        {
+    {
     while [[ -z $password_result ]] || [[ $password_result == "1" ]] ; do
         rootpasswd1=$(whiptail --passwordbox "Enter new password for root:" 10 60 3>&1 1>&2 2>&3)
         rootpasswd2=$(whiptail --passwordbox "Repeat new password for root:" 10 60 3>&1 1>&2 2>&3)
@@ -91,11 +91,44 @@ function SetRootPW
         echo -e "XXX\n100\nEnable Root Complete... \nXXX"
             sleep 0.5
 
-        } | whiptail --gauge "Please wait..." 6 60 0
+    } | whiptail --gauge "Please wait..." 6 60 0
 }
 
+function SetupLoginScreen {
+    {
+    #mkdir -m $INSTALL_DIR
+    echo -e "XXX\n10\nCheck install dir... \nXXX"
+
+    } | whiptail --gauge "Please wait..." 6 60 0
+}
+
+# Function to setup the Hostname of the Raspberry
+function SetupI2C {
+    {
+    sleep 0.5
+    #mkdir -m $INSTALL_DIR
+    echo -e "XXX\n10\nSetup PIP I2C... \nXXX"
+    #apt-get install python3-pip -y
+    echo -e "XXX\n20\nSetup I2C Tools... \nXXX"
+    #apt-get install i2c-tools -y
+    echo -e "XXX\n30\nSetup RPI.GPIO... \nXXX"
+    #pip3 install RPI.GPIO
+    echo -e "XXX\n40\nSetup ADAFruit Blinka... \nXXX"
+    #pip3 install adafruit-blinka
+    echo -e "XXX\n50\nSetup ADAFruit SSD1306... \nXXX"
+    #pip3 install adafruit-circuitpython-ssd1306
+    echo -e "XXX\n60\nSetup Python Pil... \nXXX"
+    #apt-get install python3-pil -y
+    echo -e "XXX\n70\nCopy Files... \nXXX"
+    #cp -u stats.py $INSTALL_DIR
+    echo -e "XXX\n100\nDone \nXXX"
+    sleep 1
+	} | whiptail --gauge "Please wait..." 6 60 0
+}
+
+
 #Start Setup Tool
-whiptail --title "PI Setup" --msgbox "This is a simple support tool to help setup Raspberry Pi." 8 78
+whiptail --title "PI Setup" --msgbox "This is a simple support tool to help setup Raspberry Pi.\n Details can be found in the GitHub Repo" 10 78 
 
 while [ 1 ]
 do
@@ -103,7 +136,7 @@ do
 CHOICE=$(
 whiptail --title "Pi Setup - Simplify Setup your Raspberry" --menu "Make your choice" 16 100 9 \
 	"1)" "Start Initialization"  \
-	"2)" "Enable Screen Output"  \
+	"2)" "Enable I2C Screen Output"  \
 	"3)" "End script"  3>&2 2>&1 1>&3	
 )
 #Selection
@@ -131,18 +164,26 @@ case $CHOICE in
 
         #Do main manipulation
         #Inform user about start of script
-        whiptail --title "Initialization" --msgbox "The system will now setup new boot screen. Please confirm." 8 78
-        
+        whiptail --title "Initialization" --msgbox "The system will now setup the following parts: Please confirm." 8 78
+        SetupLoginScreen
 	;;
 	"2)")   
-	    OP=$(uptime | awk '{print $3;}')
-		result="This system has been up $OP minutes"
+	    whiptail --title "Setup Screen Output" --msgbox "The system will now setup I2C Screen Output. Please confirm." 8 78
+        SetupI2C
+        whiptail --title "Setup Screen Output" --msgbox "All files are setup. You can now attach the OLED Display and reboot your system." 8 78
 	;;
 
 	"3)") 
-        clear
-        exit
-        ;;
+        if (whiptail --title "Reboot" --yesno "After you finish the setup you should reboot your PI to enable all settings made (Please remember your fix IP if set during setup). \n\nDo you want to reboot now?" 12 78); then
+            clear
+            echo "::: Reboot confirmed."
+            #reboot
+        else
+            clear
+            echo "::: Setup completed. Reboot cancelled. Please reboot manually."
+            exit
+        fi
+    ;;
 esac
 done
 exit
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ## Raspberry Pi Setup Script
 
-[![Maintenance(https://img.shields.io/badge/Maintained%3F-yes-green.svg)]
+![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg) ![Made with Bash](https://img.shields.io/badge/Made%20with-Bash-1f425f.svg) ![Made for Raspberry](https://img.shields.io/badge/-Raspberry%20Pi-C51A4A) 
 
-This simple bash script is to make some tasks to setup a Raspberry Pi more comfortable. It was created because I am using a lot of Raspberry Pis as a small server stack (like a Raspberry datacenter :computer:) and initialize Raspberry Pis regullary where different setup steps are necessary. This script just makes life easier :nerd_face: 
+This simple bash script is to make the setup of a Raspberry Pi more comfortable. It's no alternative to the official **raspi-config** tool but could simplify and shorten specific setup. It was customized and created because I am using a lot of Raspberry Pis as a small server stack (like a Raspberry datacenter :computer: ) and initialize Pis regullary where different setup steps are necessary. This script just makes life easier :nerd_face: 
 
 ## Usage :dizzy:
-To use this script just download the script and run as initial user pi
+To use this script just download the script from **Github** and run as initial user **pi**
 
 ```
 wget https://github.com/jgeisslinger/Pi-Setup/archive/master.zip
@@ -16,21 +16,21 @@ sudo Installer.sh
 ```
 
 ## Features :bulb:
-In this script the following can be done:
+With this script the following steps are done via whiptail:
 
-* Clean all non needed start scripts from standard installer
-* Set a hostname
-* Set a static network address (optional)
-* Enable root ssh access (optional)
-* Setup new login screen as shown below
-* Prepare I2C Display with standard information (see Example)
+* Clean all non needed start scripts from standard Pi installer
+* Set a new **hostname**
+* Set a **static network address** (optional)
+* Enable **root ssh** access (optional)
+* Setup new **ssh login screen** as shown below
+* Prepare **I2C Display** with standard information (see Example)
 
 ## Examples :mag:
 New boot screen once you login to your Raspberry:
 
 
 ## Credits :thumbsup:
-The script is based on various parts I found during my work on Raspberry Datacenter. Credits go to:
+The script is just a wrapper on various parts I found during my work on Raspberry Datacenter. Credits go to especially:
 
-* **Boot Screen** is a modified version of 
-* I2C Display Phyton script is based on 
+* **Boot Screen** and **I2C Pyhton** are modified versions of scripts made by Sebastian from 
+* Some snippets from **raspi-config** tool are re-used
